11cf0a83d734b351a79ed7e8fc9f83215b7b76ff
Baku <bktbkzmbkv@gmail.com>
2023-03-11 23:52:19 +0600

    added sensor service

---
+++ AirKG/AirKG/Services/SensorService.cs
@@ -0,0 +1,15 @@
+﻿using AirKG.Context;
+using AirKG.Entity;
+using AirKG.Services.Bases;
+using AutoMapper;
+
+
+namespace AirKG.Services
+{
+    public class SensorService : EntityService<Sensor>
+    {
+        public SensorService(AirKGDbContext context, IMapper mapper) : base(context, mapper) { }
+
+
+    }
+}
