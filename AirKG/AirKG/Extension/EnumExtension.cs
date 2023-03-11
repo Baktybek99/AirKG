5db7fbea7d04af808f2d9a1695f936f77bd24dbd
Baku <bktbkzmbkv@gmail.com>
2023-03-12 00:16:29 +0600

    Added enum extension

---
+++ AirKG/AirKG/Extension/EnumExtension.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+namespace AirKG.Extension
+{
+    public static class EnumExtension
+    {
+        public static string Description<T>(this T enumerationValue) where T : System.Enum
+        {
+            var defaultDescription = enumerationValue.ToString();
+            var memberInfo = enumerationValue.GetType().GetMember(defaultDescription);
+            if (memberInfo.Length > 0)
+            {
+                var attrs = memberInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (attrs.Length > 0)
+                    return ((DescriptionAttribute)attrs[0]).Description;
+            }
+
+            return defaultDescription;
+        }
+    }
+}
