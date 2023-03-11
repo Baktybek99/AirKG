d64267928b269b8c806b8ebd5ffbd8a019a2c24c
Baku <bktbkzmbkv@gmail.com>
2023-03-12 05:20:55 +0600

    added sensor view model

---
+++ AirKG/AirKG/ViewModels/SenserViewModel.cs
@@ -0,0 +1,33 @@
+﻿using AirKG.Enum;
+using System;
+
+namespace AirKG.ViewModels
+{
+    public class SensorVM
+    {
+        /// <summary>
+        ///  Id Сенсера
+        /// </summary>
+        public int Id { get ; set; }  
+
+        /// <summary>
+        /// Долгота
+        /// </summary>
+        public int Longitude { get; set; }
+
+        /// <summary>
+        /// Широта
+        /// </summary>
+        public int Latitude { get; set; }
+
+        /// <summary>
+        /// Координаты по высоте
+        /// </summary>
+        public decimal HeightCoordinates { get; set; }
+        
+        /// <summary>
+        /// Качество воздуха
+        /// </summary>
+        public double? AirQuality { get; set; }
+    }
+}
