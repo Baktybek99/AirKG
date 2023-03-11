85dd84280b2b23bfd5fbfc47cffacc6872058226
Baku <bktbkzmbkv@gmail.com>
2023-03-12 05:21:33 +0600

    added map model and controller

---
+++ AirKG/AirKG/Controllers/MapController.cs
@@ -0,0 +1,37 @@
+﻿using AirKG.Context;
+using AirKG.Entity;
+using AirKG.ViewModels;
+using AirKG.ViewModels.Markers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace AirKG.Controllers
+{
+    public class MapController : Controller
+    {
+        private readonly AirKGDbContext _context;
+        public MapController (AirKGDbContext context)
+        {
+            _context = context;
+        }
+        
+        public ActionResult Index()
+        {
+            var model = new MapModel();
+            model.Sensors = _context.Set<Sensor>()
+                .Include(x => x.AirDatas)
+                .ToList()
+                .Select(x => new SensorVM
+                {
+                    Id = x.Id,
+                    AirQuality = x.AirDatas?.OrderByDescending(x => x.DateCreate)?.First()?.AirQuality,
+                    HeightCoordinates = x.HeightCoordinates,
+                    Latitude= x.Latitude,
+                    Longitude= x.Longitude
+                })
+                .ToList();
+            return View(model);
+        }
+    }
+}
---
+++ AirKG/AirKG/ViewModels/Markers/MapModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System;
+using AirKG.Entity;
+using System.Security.Permissions;
+
+namespace AirKG.ViewModels.Markers
+{
+    public class MapModel
+    {
+        public List<Tuple<int, int, string>> Markers { get; set; }
+
+        public IEnumerable<SensorVM> Sensors { get; set; }
+        
+
+        public MapModel()
+        {
+            Markers = new List<Tuple<int, int, string>>();
+            Markers.Add(Tuple.Create(100, 200, "https://example.com/street1"));
+            Markers.Add(Tuple.Create(300, 400, "https://example.com/street2"));
+        }
+    }
+}
