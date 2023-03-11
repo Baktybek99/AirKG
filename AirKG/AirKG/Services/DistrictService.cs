ad4d0d7885276647b9de5d9befafb69b918cbbd7
Baku <bktbkzmbkv@gmail.com>
2023-03-11 23:53:53 +0600

    added district service

---
+++ AirKG/AirKG/Services/DistrictService.cs
@@ -0,0 +1,12 @@
+﻿using AirKG.Context;
+using AirKG.Entity;
+using AirKG.Services.Bases;
+using AutoMapper;
+
+namespace AirKG.Services
+{
+    public class DistrictService : EntityService<District>
+    {
+        public DistrictService(AirKGDbContext context, IMapper mapper) : base(context, mapper) { }
+    }
+}
