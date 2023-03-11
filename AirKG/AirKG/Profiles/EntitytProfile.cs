a0a3c973ba5874795c89dce74136038e17f6bfec
Baku <bktbkzmbkv@gmail.com>
2023-03-12 00:17:16 +0600

    added mapster profile

---
+++ AirKG/AirKG/Profiles/EntitytProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+
+namespace AirKG.Profiles
+{
+    public class EntitytProfile : Profile
+    {
+        public EntitytProfile()
+        {
+            ///ПРимер создания
+            //CreateMap<T,TEntity>()
+            //    .ForMember();
+        }
+    }
+}
