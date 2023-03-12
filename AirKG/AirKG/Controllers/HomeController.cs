02e8e67656c09e5a452741a7c5cd762c293b6e1a
Baku <bktbkzmbkv@gmail.com>
2023-03-12 08:15:57 +0600

    Added Cart route Added Cart view

--- AirKG/AirKG/Controllers/HomeController.cs
+++ AirKG/AirKG/Controllers/HomeController.cs
@@ -29,5 +29,10 @@ namespace AirKG.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
     }
 }
