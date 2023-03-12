386d8625171e1fa9f903b2f8d1247b7dfd1e9baf
Baku <bktbkzmbkv@gmail.com>
2023-03-12 08:16:33 +0600

    added new migrations

---
+++ AirKG/AirKG/Migrations/20230311181301_Update_Sensor.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirKG.Migrations
+{
+    public partial class Update_Sensor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Longitude",
+                table: "Sensors",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Latitude",
+                table: "Sensors",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "HeightCoordinates",
+                table: "Sensors",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "СoordinatesInTheSide",
+                table: "Sensors",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HeightCoordinates",
+                table: "Sensors");
+
+            migrationBuilder.DropColumn(
+                name: "СoordinatesInTheSide",
+                table: "Sensors");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Longitude",
+                table: "Sensors",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Latitude",
+                table: "Sensors",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
