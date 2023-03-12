a9dbe7429e4d9ce4c1abef98e7fc726b7cf1b20d
Baku <bktbkzmbkv@gmail.com>
2023-03-12 10:45:52 +0600

    changed relize map method

--- AirKG/AirKG/Controllers/MapController.cs
+++ AirKG/AirKG/Controllers/MapController.cs
@@ -2,6 +2,7 @@
 using AirKG.Entity;
 using AirKG.ViewModels;
 using AirKG.ViewModels.Markers;
+using AutoMapper.Configuration.Conventions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
@@ -28,7 +29,89 @@ namespace AirKG.Controllers
                     AirQuality = x.AirDatas?.OrderByDescending(x => x.DateCreate)?.First()?.AirQuality,
                     HeightCoordinates = x.HeightCoordinates,
                     Latitude= x.Latitude,
-                    Longitude= x.Longitude
+                    Longitude= x.Longitude,
+                    PlaceName= x.PlaceName
+
+                })
+                .ToList();
+            return View(model);
+        }
+
+        public ActionResult Index1()
+        {
+            var model = new MapModel();
+            model.Sensors = _context.Set<Sensor>()
+                .Include(x => x.AirDatas)
+                .ToList()
+                .Select(x => new SensorVM
+                {
+                    Id = x.Id,
+                    AirQuality = x.AirDatas?.OrderByDescending(x => x.DateCreate)?.First()?.AirQuality,
+                    HeightCoordinates = x.HeightCoordinates,
+                    Latitude = x.Latitude,
+                    Longitude = x.Longitude,
+                    PlaceName = x.PlaceName
+
+                })
+                .ToList();
+            return View(model);
+        }
+
+        public ActionResult Index2()
+        {
+            var model = new MapModel();
+            model.Sensors = _context.Set<Sensor>()
+                .Include(x => x.AirDatas)
+                .ToList()
+                .Select(x => new SensorVM
+                {
+                    Id = x.Id,
+                    AirQuality = x.AirDatas?.OrderByDescending(x => x.DateCreate)?.First()?.AirQuality,
+                    HeightCoordinates = x.HeightCoordinates,
+                    Latitude = x.Latitude,
+                    Longitude = x.Longitude,
+                    PlaceName = x.PlaceName
+
+                })
+                .ToList();
+            return View(model);
+        }
+
+
+        public ActionResult Index3()
+        {
+            var model = new MapModel();
+            model.Sensors = _context.Set<Sensor>()
+                .Include(x => x.AirDatas)
+                .ToList()
+                .Select(x => new SensorVM
+                {
+                    Id = x.Id,
+                    AirQuality = x.AirDatas?.OrderByDescending(x => x.DateCreate)?.First()?.AirQuality,
+                    HeightCoordinates = x.HeightCoordinates,
+                    Latitude = x.Latitude,
+                    Longitude = x.Longitude,
+                    PlaceName = x.PlaceName
+
+                })
+                .ToList();
+            return View(model);
+        }
+        public ActionResult Index4()
+        {
+            var model = new MapModel();
+            model.Sensors = _context.Set<Sensor>().Where(x => x.PlaceName == "Ленин")
+                .Include(x => x.AirDatas)
+                .ToList()
+                .Select(x => new SensorVM
+                {
+                    Id = x.Id,
+                    AirQuality = x.AirDatas?.OrderByDescending(x => x.DateCreate)?.First()?.AirQuality,
+                    HeightCoordinates = x.HeightCoordinates,
+                    Latitude = x.Latitude,
+                    Longitude = x.Longitude,
+                    PlaceName = x.PlaceName
+
                 })
                 .ToList();
             return View(model);
